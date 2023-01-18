6979d1b31e1e9566e40f541c2672876bf8f1fdc6
jasondoss <jason.j.doss@gmail.com>
2023-01-17 16:55:22 -0800

    added a thought.js and cleaned up code.

--- models/User.js
+++ models/User.js
@@ -1,41 +1,41 @@
 const { Schema, model } = require('mongoose');
 
 const userSchema = new Schema(
-{
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trimmed: true,
-  },
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
 
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    match: [/.@+\..+/, 'Must match an email address!'],
-  },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.@+\..+/, 'Must match an email address!'],
+    },
 
-  thoughts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Thought',
-    }
- ],
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      }
+    ],
 
- friends: [
-   {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-   },
- ],
-},
-{
-  toJSON: {
-    virtuals: true
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
-  id: false,
- }
+  {
+    toJSON: {
+      virtuals: true
+    },
+    id: false,
+  }
 );
 
 userSchema.virtual('friendCount').get(function () {
